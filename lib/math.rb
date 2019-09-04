6bde82bb072a18f91384f3308c455794de6b7468
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-04 21:49:58 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,18 @@
 def addition(num1, num2)
-
+  addition = num1 + num2
 end
-
 def subtraction(num1, num2)
-
+  substraction = num2 - num1
 end
-
 def division(num1, num2)
-
+  division = num2 / num1
 end
-
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
 end
-
 def modulo(num1, num2)
-
+  modulo = % (num1, num2)
 end
-
 def square_root(num)
-
+  square_root = Math.sqrt (num)
 end
